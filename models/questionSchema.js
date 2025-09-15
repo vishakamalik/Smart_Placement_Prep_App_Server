2b68982c4831b835477e146e55b54f221c5af223
Vishaka Malik <vishakamalik13@gmail.com>
2025-09-15 10:40:13 +0530

    Updated App_Server code

--- models/questionSchema.js
+++ models/questionSchema.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 /** question model */
 const questionModel = new Schema({
     questions: { type : Array, default: []}, // create question with [] default value
-    answers : { type : Array, default: []},
+    // answers : { type : Array, default: []},
     createdAt: { type: Date, default: Date.now },
 });
 
